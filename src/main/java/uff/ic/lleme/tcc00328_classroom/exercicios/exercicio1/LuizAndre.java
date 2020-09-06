1c9f63b1ed5c5a6b9c16ff0dc7a2db008b2a5e26
lapaesleme <lapaesleme@ic.uff.br>
2020-09-06 11:59:30 -0300

    Create LuizAndre.java

---
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/exercicios/exercicio1/LuizAndre.java
@@ -0,0 +1,8 @@
+package uff.ic.lleme.tcc00328_classroom.exercicios.exercicio1;
+
+public class LuizAndre {
+
+    public static void main(String[] args) {
+        System.out.println("Alô mundo!");
+    }
+}
