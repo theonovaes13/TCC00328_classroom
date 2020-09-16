6d1ac406d09520427d9972377e10fbb4c83236a5
lapaesleme <lapaesleme@ic.uff.br>
2020-09-16 13:19:22 -0300

    ok

--- src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio1/LuizAndre.java
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio1/LuizAndre.java
@@ -3,6 +3,12 @@ package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio1;
 public class LuizAndre {
 
     public static void main(String[] args) {
-        System.out.println("Alô mundo!");
+        int x = 5;
+        int n = 1;
+        x = 5 + n++;
+        System.out.println(x);
+        n = 1;
+        x = 5 + ++n;
+        System.out.println(x);
     }
 }
