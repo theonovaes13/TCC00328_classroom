10098a8d1038e299b31f55afa2742cd82d5bdb7b
GuiTomaz <lhermetomaz@gmail.com>
2020-09-16 16:59:15 -0300

    Exercicio 5 - Guilherme

---
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio5/exercicio5.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GuiTomaz
+ */
+public class exercicio5 {
+    public static void main(String[]args){
+        int num;
+        Scanner tecla = new Scanner(System.in);
+        System.out.println("Informe um numero n para saber qual é este enesimo termo da sequencia fibonacci: ");
+        num = tecla.nextInt();
+        int resultado = termo(num);
+        System.out.println(num+"º termo da sequencia fibonacci " + resultado);
+        
+    }
+    public static int termo(int num){
+        int termoN;
+        if(num == 1 || num == 0){
+            termoN = num;
+        }else{
+            termoN = termo(num-1) + termo(num-2);
+        }
+        return termoN;
+    }
+}
