3395947ab36b680d25125858105c44173b8732e3
lapaesleme <lapaesleme@ic.uff.br>
2020-09-18 15:42:20 -0300

    Update TheoNovaesMellodeOliveira.java

--- src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio1/TheoNovaesMellodeOliveira.java
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio1/TheoNovaesMellodeOliveira.java
@@ -1,7 +1,16 @@
-package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio1;
-public class TheoNovaesMellodeOliveira {
 
+package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio1;
+
+/**
+ *
+ */
+public class TheoNovaesMelloDeOliveira {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Alô mundo.");
+      System.out.println("Hello World!"); // Display the string.
     }
+    
 }
