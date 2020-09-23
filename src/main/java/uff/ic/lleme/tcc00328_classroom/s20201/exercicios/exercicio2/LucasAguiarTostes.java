1b3b38e56f8e524be8967a25293f883a87d28d79
Luiz Leme <lapaesleme@ic.uff.br>
2020-09-23 14:56:54 -0300

    ok

--- src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio2/LucasAguiarTostes.java
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio2/LucasAguiarTostes.java
@@ -1,25 +1,22 @@
 package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio2;
-import java.util.Scanner;
-
 
 public class LucasAguiarTostes {
-	public static void main(String[] args) {
-		int num=4;
-		System.out.println("o fatorial é: "+fatorial(num));
-		num= 7;
-		System.out.println("o fatorial é: "+fatorial(num));
-		num=1;
-		System.out.println("o fatorial é: "+fatorial(num));
-	}
-	
-	public static int fatorial(int num){
-		int resultado = 1;	
-		while(num>0) {
-			resultado *= num;
-			num--;
-		}
-		return resultado;
-	}
+
+    public static void main(String[] args) {
+        int num = 4;
+        System.out.println("o fatorial e: " + fatorial(num));
+        num = 7;
+        System.out.println("o fatorial e: " + fatorial(num));
+        num = 1;
+        System.out.println("o fatorial e: " + fatorial(num));
+    }
+
+    public static int fatorial(int num) {
+        int resultado = 1;
+        while (num > 0) {
+            resultado *= num;
+            num--;
+        }
+        return resultado;
+    }
 }
-
-
--- src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio5/LucasAguiarTostes.java
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio5/LucasAguiarTostes.java
@@ -1,39 +1,32 @@
 package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio5;
 
 public class LucasAguiarTostes {
-	
-	public static void main(String[] args) {
-		int num = 5;
-		System.out.println("o número é:"+fibonacci(num));
-		
-		num = 7;
-		System.out.println("o número é:"+fibonacci(num));
-		
-		num = 12;
-		System.out.println("o número é:"+fibonacci(num));
-	}
-	
-	
-	
-	
-	public static int fibonacci(int num) {
-		int val1=0, val2=1,val3=0, count=2;
-		if (num==0) {
-			return 0;
-		}
-		if (num==1) {
-			return 1;
-		}
-		while(count<num) {
-			val3 = val1 + val2;
-			val1=val2;
-			val2=val3;
-			count++;
-		}
-		
-		
-		
-		return val3;
-	}
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("o numero e:" + fibonacci(num));
+
+        num = 7;
+        System.out.println("o numero e:" + fibonacci(num));
+
+        num = 12;
+        System.out.println("o numero e:" + fibonacci(num));
+    }
+
+    public static int fibonacci(int num) {
+        int val1 = 0, val2 = 1, val3 = 0, count = 2;
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+        while (count < num) {
+            val3 = val1 + val2;
+            val1 = val2;
+            val2 = val3;
+            count++;
+        }
+
+        return val3;
+    }
 
 }
