b1fd2339fdf57ca18b4ed8b0ab2ed599320e173e
Luiz Leme <lapaesleme@ic.uff.br>
2020-09-23 16:33:13 -0300

    ok

--- src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/Rascunho.java
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/Rascunho.java
@@ -3,6 +3,20 @@ package uff.ic.lleme.tcc00328_classroom.s20201.exercicios;
 public class Rascunho {
 
     public static void main(String[] args) {
-        System.out.println(-42 % -24);
+        System.out.println("1 e primo: " + isPrime(1));
+        System.out.println("2 e primo: " + isPrime(2));
+        System.out.println("221 e primo: " + isPrime(221));
+        System.out.println("227 e primo: " + isPrime(227));
+        System.out.println("229 e primo: " + isPrime(229));
+    }
+
+    public static boolean isPrime(int n) {
+        if (n > 1) {
+            for (int div = 2; n > 2 && div < Math.sqrt(n) + 1; div++)
+                if (n % div == 0)
+                    return false;
+            return true;
+        } else
+            return false;
     }
 }
