d745ec891a3952467004e104860f2dbbf33d0684
Gyselle Mello <gysellemello@id.uff.br>
2020-09-22 22:03:53 -0300

    Create GyselleMello.java
    Resolução do Exercício 13

---
+++ src/main/java/uff/ic/lleme/tcc00328_classroom/s20201/exercicios/exercicio13/GyselleMello.java
@@ -0,0 +1,22 @@
+package uff.ic.lleme.tcc00328_classroom.s20201.exercicios.exercicio13;
+import java.util.Scanner;
+
+public class GyselleMello {
+    public static void main(String[] args) {
+        int n, i;
+        float valor, media = 0;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe o número de valores que serão fornecidos:");
+        n = entrada.nextInt();
+        
+        for(i=1;i<=n;i++) {
+            System.out.println("Digite um valor real:");
+            valor = entrada.nextFloat();
+            media += valor;
+        }
+        
+        media = media/n;
+        System.out.println("A média dos valor é: " + media);
+    }
+}
